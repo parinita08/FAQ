9d966cc8eaf87cd25b71842711293cc0d186c0b6
Parinita Vedantam <vedantampari@gmail.com>
2020-09-19 18:04:25 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// function to select elements
+const selectElement = (s) => document.querySelector(s);
+
+// Open the menu on click
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+// Close the menu click
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
